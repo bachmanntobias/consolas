0116158c9fee769ed43f5ba2c7775e89c471343a
Tobias Bachmann <bachmanntobias96@gmail.com>
2018-03-16 14:15:41 +0100

    Signed-off-by: Tobias Bachmann <bachmanntobias96@gmail.com>
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   App.config
    	new file:   GradeBook.cs
    	new file:   Program.cs
    	new file:   Properties/AssemblyInfo.cs
    	new file:   bin/Debug/consolas.exe.config
    	new file:   consolas.csproj
    	new file:   obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs
    	new file:   obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs
    	new file:   obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs
    	new file:   obj/Debug/consolas.csproj.FileListAbsolute.txt

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+
+            book = new GradeBook();
+            book.AddGrade(75);
+        }
+
+    }
+}
