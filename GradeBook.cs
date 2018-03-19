100a494ce1b21e6c381cf212c5b27cbdbcb26dea
Tobias Bachmann <bachmanntobias96@gmail.com>
2018-03-19 14:18:03 +0100

    re
    Signed-off-by: Tobias Bachmann <bachmanntobias96@gmail.com>

--- GradeBook.cs
+++ GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +11,33 @@ namespace Grades
     {
         public GradeBook()
         {
+            _name = "Empty";
             grades = new List<float>();
         }
 
         public GradeStatistics ComputeStatistics()
         {
             GradeStatistics stats = new GradeStatistics();
-            
-            float sum = 0; 
-            foreach(float grade in grades)
+
+            float sum = 0;
+            foreach (float grade in grades)
             {
                 stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
                 stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
                 sum += grade;
             }
 
-            stats.AverageGrade = sum / grades.Count; 
+            stats.AverageGrade = sum / grades.Count;
             return stats;
         }
- 
+
+        public void WriteGrades(TextWriter destination)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                destination.WriteLine(grades[i]);
+            }
+        }
 
         public void AddGrade(float grade)
         {
@@ -39,17 +48,31 @@ namespace Grades
         {
             get
             {
-                return _name; 
+                return _name;
             }
             set
             {
-                if (!String.IsNullOrEmpty(value))
+                if (String.IsNullOrEmpty(value))
                 {
-                    _name = value;
+                    throw new ArgumentException("Name cannot be empty2");
                 }
+
+
+                if (_name != value && NameChanged != null)
+                {
+                    NameChangedEventArgs args = new NameChangedEventArgs();
+                    args.ExistingName = _name;
+                    args.NewName = value;
+
+                    NameChanged(this, args);
+                }
+                _name = value;
+
             }
         }
+        public event NameChangedDelegate NameChanged;
+        //asterisks
         private String _name;
-        private List<float> grades; 
+        private List<float> grades;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,34 +11,58 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+
+
+
             GradeBook book = new GradeBook();
 
-            book.Name = "Scott's Gradebook";
-            book.Name = null;
+            
+            try
+            {
+
+                Console.WriteLine("Enter name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex )
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception");
+            }
+
             book.AddGrade(75);
             book.AddGrade(91);
             book.AddGrade(89.5f);
+            StreamWriter outputFile = File.CreateText("grades.txt");
+            book.WriteGrades(Console.Out);
+
 
             GradeStatistics stats = book.ComputeStatistics();
             Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest", (int)stats.AverageGrade);
+            WriteResult("Highest", stats.AverageGrade);
             String m = stats.AverageGrade.ToString(); 
             WriteResult("Lowest", m );
-
-            GradeBook book2 = book;
-            book2.AddGrade(75);
+            WriteResult(stats.Description, stats.LetterGrade);
+          
+            
         }
+
+        
+               
 
         static void WriteResult (String description, float result)
         {
-            Console.WriteLine($"{description}{" : "}{result:C1}");
+            Console.WriteLine($"{description}{" : "}{result}");
         }
 
-        static void WriteResult(String description, int result)
-        {
-            Console.WriteLine(description + " : " + result);
-        }
+
         static void WriteResult(String description, String result)
         {
             Console.WriteLine(description + " : " + result);
